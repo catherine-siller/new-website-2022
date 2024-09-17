e5519863a97ae3972f18db372c31da40a60554a3
Catherine Siller <cathysiller503@gmail.com>
2024-09-17 16:52:44 -0500

    website cleanup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Helmet} from "react-helmet";
-import SVG from 'react-inlinesvg'
+import React from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import SVG from "react-inlinesvg";
 
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 //import CatherineSillerResume from './assets/Catherine_Siller_Resume.pdf'
-import GithubIcon from './assets/svg/github.svg'
-import LinkedinIcon from './assets/svg/linkedin.svg'
-import favicon from './assets/images/favicon.ico'
+import GithubIcon from "./assets/svg/github.svg";
+import LinkedinIcon from "./assets/svg/linkedin.svg";
+import favicon from "./assets/images/favicon.ico";
 
 const TopLine = styled.div`
-  background: linear-gradient(to bottom, #fcecfc 0%,#fba6e1 50%,#fd89d7 83%,#ff7cd8 100%);
+  background: linear-gradient(
+    to bottom,
+    #fcecfc 0%,
+    #fba6e1 50%,
+    #fd89d7 83%,
+    #ff7cd8 100%
+  );
   left: 0;
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 100;
-`
+`;
 
 const Contact = styled.div`
   display: inline-flex;
-`
+`;
 
 const Button = styled.div`
-  border: 2px solid #1098AD;
+  border: 2px solid #1098ad;
   display: block;
   cursor: pointer;
   padding: 16px 32px;
   font-weight: 600;
   font-size: 16px;
   text-transform: lowercase;
-`
+`;
 
 const Section = styled.section`
   color: #444444;
-  font-family: "Poppins",sans-serif;
+  font-family: "Poppins", sans-serif;
   margin: 64px auto;
-  max-width: 800px;
-  padding-top: 24px;
+  //padding-top: 24px;
+  //padding: 24px 48px 0;
   text-align: center;
   width: 100%;
+  overflow: hidden;
 
   > div.top {
     display: block;
@@ -58,7 +65,17 @@ const Section = styled.section`
       > span {
         -webkit-background-clip: text;
         -webkit-text-fill-color: #0000;
-        background-image: linear-gradient(90deg, #F79533 0%, #F37055 15%, #EF4E7B 30%, #A166AB 44%, #5073B8 58%, #1098AD 72%, #07B39B 86%, #6DBA82 100%);
+        background-image: linear-gradient(
+          90deg,
+          #f79533 0%,
+          #f37055 15%,
+          #ef4e7b 30%,
+          #a166ab 44%,
+          #5073b8 58%,
+          #1098ad 72%,
+          #07b39b 86%,
+          #6dba82 100%
+        );
         background-size: cover;
       }
     }
@@ -70,10 +87,12 @@ const Section = styled.section`
 
     p {
       font-size: 18px;
-      margin: 0 32px;
+      max-width: 800px;
+      margin: auto auto;
+      word-wrap: break-word;
 
       &:not(:first-of-type) {
-        margin-top: 8px;
+        padding-top: 16px;
       }
     }
 
@@ -88,7 +107,17 @@ const Section = styled.section`
     a:-webkit-any-link {
       -webkit-background-clip: text;
       -webkit-text-fill-color: #0000;
-      background-image: linear-gradient(90deg, #F79533 0%, #F37055 15%, #EF4E7B 30%, #A166AB 44%, #5073B8 58%, #1098AD 72%, #07B39B 86%, #6DBA82 100%);
+      background-image: linear-gradient(
+        90deg,
+        #f79533 0%,
+        #f37055 15%,
+        #ef4e7b 30%,
+        #a166ab 44%,
+        #5073b8 58%,
+        #1098ad 72%,
+        #07b39b 86%,
+        #6dba82 100%
+      );
       background-size: cover;
     }
 
@@ -101,86 +130,174 @@ const Section = styled.section`
     }
 
     @-webkit-keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
 
     @-moz-keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
 
     @keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
   }
-`
+`;
 
 function App() {
   return (
     <>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Catherine Siller's Portfolio</title>
-      <link rel="canonical" href="http://www.catherine-siller.com/" />
-      <link rel="icon" href={favicon} />
-    </Helmet>
-    <TopLine />
-    <Header />
-    <Section>
-      <div className="top">
-        <img
-          alt="Catherine Siller"
-          src={require('./assets/images/avatar.png')}
-        />
-        <a href="https://github.com/cathysiller" class="icon" target="_blank" rel="noreferrer" title="My GitHub" aria-label="My GitHub">
-          <SVG src={GithubIcon} />
-        </a>
-        <a href="https://www.linkedin.com/in/catherinesiller/" target="_blank" rel="noreferrer" title="My Linkedin" aria-label="Linkedin">
-          <SVG src={LinkedinIcon} />
-        </a>
-        <h1>Hi, I'm <span>Catherine Siller — </span></h1>
-        <h2>Senior Software Developer</h2>
-        <p>
-          Previous senior software engineer at <a href="https://www.hulu.com/welcome" target="_blank" rel="noreferrer">@Hulu</a> and
-          <a href="https://www.mailgun.com/" target="_blank" rel="noreferrer">@Mailgun</a>.I help small businesses and growing companies build sustainable, enduring user experiences.</p>
-        <p>Currently accepting freelance projects, <a href="mailto:catsiller12@gmail.com">email me!</a></p>
-      </div>
-    </Section>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Catherine Siller's Portfolio</title>
+        <link rel="canonical" href="http://www.catherine-siller.com/" />
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <TopLine />
+      <Header />
+      <Section>
+        <div className="top">
+          <img
+            alt="Catherine Siller"
+            src={require("./assets/images/avatar.png")}
+          />
+          <a
+            href="https://github.com/cathysiller"
+            class="icon"
+            target="_blank"
+            rel="noreferrer"
+            title="My GitHub"
+            aria-label="My GitHub"
+          >
+            <SVG src={GithubIcon} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/catherinesiller/"
+            target="_blank"
+            rel="noreferrer"
+            title="My Linkedin"
+            aria-label="Linkedin"
+          >
+            <SVG src={LinkedinIcon} />
+          </a>
+          <h1>
+            Hi, I'm <span>Catherine Siller</span>
+          </h1>
+          <h2>Senior Software Developer</h2>
+          <p>
+            Previous senior software engineer at{" "}
+            <a
+              href="https://www.hulu.com/welcome"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @Hulu
+            </a>{" "}
+            and
+            <a href="https://www.mailgun.com/" target="_blank" rel="noreferrer">
+              @Mailgun
+            </a>
+            . I also help small businesses build sustainable and enduring user
+            experiences.
+          </p>
+          <p>
+            Currently accepting freelance projects,{" "}
+            <a href="mailto:catsiller12@gmail.com">email me!</a>
+          </p>
+        </div>
+      </Section>
 
-    <Section id='projects'>
-      <Projects />
-    </Section>
+      <Section id="projects">
+        <Projects />
+      </Section>
 
-    <Section id='skills'>
-      <Skills />
-    </Section>
+      <Section id="skills">
+        <Skills />
+      </Section>
 
-    <Section id='contact'>
-      <h1>Contact</h1>
-      <Contact>
-        <Button>
-          <a href="mailto:catsiller12@gmail.com">Email Me!</a>
-        </Button>
-      </Contact>
-    </Section>
+      <Section id="contact">
+        <h1>Contact</h1>
+        <Contact>
+          <Button>
+            <a href="mailto:catsiller12@gmail.com">Email Me!</a>
+          </Button>
+        </Contact>
+      </Section>
 
-    <Section>
-      <Footer />
-    </Section>
+      <Section>
+        <Footer />
+      </Section>
     </>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Helmet} from "react-helmet";
-import SVG from 'react-inlinesvg'
+import React from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import SVG from "react-inlinesvg";
 
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
-import GithubIcon from './assets/svg/github.svg'
-import LinkedinIcon from './assets/svg/linkedin.svg'
-import favicon from './assets/images/favicon.ico'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+import GithubIcon from "./assets/svg/github.svg";
+import LinkedinIcon from "./assets/svg/linkedin.svg";
+import favicon from "./assets/images/favicon.ico";
 
 const TopLine = styled.div`
-  background: linear-gradient(to bottom, #fcecfc 0%,#fba6e1 50%,#fd89d7 83%,#ff7cd8 100%);
+  background: linear-gradient(
+    to bottom,
+    #fcecfc 0%,
+    #fba6e1 50%,
+    #fd89d7 83%,
+    #ff7cd8 100%
+  );
   left: 0;
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 100;
-`
+`;
 
 const Contact = styled.div`
   display: inline-flex;
-`
+`;
 
 const Button = styled.div`
-  border: 2px solid #1098AD;
+  border: 2px solid #1098ad;
   display: block;
   cursor: pointer;
   padding: 16px 32px;
   font-weight: 600;
   font-size: 16px;
   text-transform: lowercase;
-`
+`;
 
 const Section = styled.section`
   color: #444444;
-  font-family: "Poppins",sans-serif;
+  font-family: "Poppins", sans-serif;
   margin: 64px auto;
   max-width: 800px;
   padding-top: 24px;
@@ -57,7 +63,17 @@ const Section = styled.section`
       > span {
         -webkit-background-clip: text;
         -webkit-text-fill-color: #0000;
-        background-image: linear-gradient(90deg, #F79533 0%, #F37055 15%, #EF4E7B 30%, #A166AB 44%, #5073B8 58%, #1098AD 72%, #07B39B 86%, #6DBA82 100%);
+        background-image: linear-gradient(
+          90deg,
+          #f79533 0%,
+          #f37055 15%,
+          #ef4e7b 30%,
+          #a166ab 44%,
+          #5073b8 58%,
+          #1098ad 72%,
+          #07b39b 86%,
+          #6dba82 100%
+        );
         background-size: cover;
       }
     }
@@ -87,7 +103,17 @@ const Section = styled.section`
     a:-webkit-any-link {
       -webkit-background-clip: text;
       -webkit-text-fill-color: #0000;
-      background-image: linear-gradient(90deg, #F79533 0%, #F37055 15%, #EF4E7B 30%, #A166AB 44%, #5073B8 58%, #1098AD 72%, #07B39B 86%, #6DBA82 100%);
+      background-image: linear-gradient(
+        90deg,
+        #f79533 0%,
+        #f37055 15%,
+        #ef4e7b 30%,
+        #a166ab 44%,
+        #5073b8 58%,
+        #1098ad 72%,
+        #07b39b 86%,
+        #6dba82 100%
+      );
       background-size: cover;
     }
 
@@ -100,86 +126,174 @@ const Section = styled.section`
     }
 
     @-webkit-keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
 
     @-moz-keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
 
     @keyframes omg {
-      0%, 100% { color: #7ccdea; fill: #7ccdea; }
-      16%      { color: #0074D9; fill: #0074D9; }
-      32%      { color: #2ECC40; fill: #2ECC40;}
-      48%      { color: #FFDC00; fill: #FFDC00;}
-      64%      { color: #B10DC9; fill: #B10DC9;}
-      80%      { color: #FF4136; fill: #FF4136;}
+      0%,
+      100% {
+        color: #7ccdea;
+        fill: #7ccdea;
+      }
+      16% {
+        color: #0074d9;
+        fill: #0074d9;
+      }
+      32% {
+        color: #2ecc40;
+        fill: #2ecc40;
+      }
+      48% {
+        color: #ffdc00;
+        fill: #ffdc00;
+      }
+      64% {
+        color: #b10dc9;
+        fill: #b10dc9;
+      }
+      80% {
+        color: #ff4136;
+        fill: #ff4136;
+      }
     }
   }
-`
+`;
 
 function App() {
   return (
     <>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Catherine Siller — Software Engineer</title>
-      <link rel="canonical" href="http://www.catherine-siller.com/" />
-      <link rel="icon" href={favicon} />
-    </Helmet>
-    <TopLine />
-    <Header />
-    <Section>
-      <div className="top">
-        <img
-          alt="Catherine Siller"
-          src={require('./assets/images/avatar.png')}
-        />
-        <a href="https://github.com/cathysiller" class="icon" target="_blank" rel="noreferrer" title="My GitHub" aria-label="My GitHub">
-          <SVG src={GithubIcon} />
-        </a>
-        <a href="https://www.linkedin.com/in/catherinesiller/" target="_blank" rel="noreferrer" title="My Linkedin" aria-label="Linkedin">
-          <SVG src={LinkedinIcon} />
-        </a>
-        <h1>Hi! I'm <span>Catherine Siller</span></h1>
-        <h2>Front End Software Developer</h2>
-        <p>
-          Front end software developer currently <a href="https://twitter.com/hulu" target="_blank" rel="noreferrer">@Hulu</a>, previously 
-          <a href="https://twitter.com/Mail_Gun" target="_blank" rel="noreferrer">@Mailgun</a>. I love all things web that are modern, fast, and pretty. I'm keeping it cute on the web and irl.</p>
-        <p>Currently accepting freelance projects, <a href="mailto:catsiller12@gmail.com">email me!</a></p>
-      </div>
-    </Section>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Catherine Siller — Software Engineer</title>
+        <link rel="canonical" href="http://www.catherine-siller.com/" />
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <TopLine />
+      <Header />
+      <Section>
+        <div className="top">
+          <img
+            alt="Catherine Siller"
+            src={require("./assets/images/avatar.png")}
+          />
+          <a
+            href="https://github.com/cathysiller"
+            class="icon"
+            target="_blank"
+            rel="noreferrer"
+            title="My GitHub"
+            aria-label="My GitHub"
+          >
+            <SVG src={GithubIcon} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/catherinesiller/"
+            target="_blank"
+            rel="noreferrer"
+            title="My Linkedin"
+            aria-label="Linkedin"
+          >
+            <SVG src={LinkedinIcon} />
+          </a>
+          <h1>
+            Hi! I'm <span>Catherine Siller</span>
+          </h1>
+          <h2>Front End Software Developer</h2>
+          <p>
+            Front end software developer currently{" "}
+            <a href="https://twitter.com/hulu" target="_blank" rel="noreferrer">
+              @Hulu
+            </a>
+            , previously
+            <a
+              href="https://twitter.com/Mail_Gun"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @Mailgun
+            </a>
+            . I love all things web that are modern, fast, and pretty. I'm
+            keeping it cute on the web and irl.
+          </p>
+          <p>
+            Currently accepting freelance projects,{" "}
+            <a href="mailto:catsiller12@gmail.com">email me!</a>
+          </p>
+        </div>
+      </Section>
 
-    <Section id='projects'>
-      <Projects />
-    </Section>
+      <Section id="projects">
+        <Projects />
+      </Section>
 
-    <Section id='skills'>
-      <Skills />
-    </Section>
+      <Section id="skills">
+        <Skills />
+      </Section>
 
-    <Section id='contact'>
-      <h1>Contact</h1>
-      <Contact>
-        <Button>
-          <a href="mailto:catsiller12@gmail.com">Email Me!</a>
-        </Button>
-      </Contact>
-    </Section>
+      <Section id="contact">
+        <h1>Contact</h1>
+        <Contact>
+          <Button>
+            <a href="mailto:catsiller12@gmail.com">Email Me!</a>
+          </Button>
+        </Contact>
+      </Section>
 
-    <Section>
-      <Footer />
-    </Section>
+      <Section>
+        <Footer />
+      </Section>
     </>
   );
 }
