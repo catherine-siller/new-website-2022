7f04c357e43a8fc377a7e45d0fc71682169af2e5
Catherine Siller <cathysiller503@gmail.com>
2024-09-19 19:04:04 -0500

    cleanup and fix broken link in nav for homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
-import Navbar from '../Navbar/Navbar'
-import './Header.css'
+import Navbar from "../Navbar/Navbar";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header className='header center'>
+    <header className="header center">
       <h3>
-        <a href="www.catherine-siller.com" className='link'>
-        CS.
+        <a href="/" className="link">
+          CS.
         </a>
       </h3>
       <Navbar />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
